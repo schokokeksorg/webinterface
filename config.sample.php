e2f6dbde315b24214b207f74f5048431154071ab
bernd <bernd@87cf0b9e-d624-0410-a070-f6ee81989793>
2007-07-30 19:06:10 +0000

    Beispiel-Config
    git-svn-id: https://svn.schokokeks.org/repos/tools/webinterface/trunk@578 87cf0b9e-d624-0410-a070-f6ee81989793

---
+++ config.sample.php
@@ -0,0 +1,20 @@
+<?php
+
+global $config;
+$config = array();
+
+$config['db_host'] = ':/var/run/mysqld/mysqld-sys.sock';
+$config['db_user'] = 'username';
+$config['db_pass'] = 'password';
+
+
+$config['modules'] = array("index", "domains", "imap", "mysql", "jabber", "vhosts", "register", "systemuser", "su");
+
+$config['cracklib_dict'] = 'inc/cracklib_dict';
+
+$config['enable_debug'] = true;
+
+
+ini_set('error_reporting','On');
+
+?>
