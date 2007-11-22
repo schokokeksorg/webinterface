6b2630544dc3c6974849c06bc77950524922060e
bernd <bernd@87cf0b9e-d624-0410-a070-f6ee81989793>
2007-11-22 12:40:55 +0000

    Javascript aus vmail-Modul entfernt
    git-svn-id: https://svn.schokokeks.org/repos/tools/webinterface/trunk@799 87cf0b9e-d624-0410-a070-f6ee81989793

--- modules/vmail/edit.php
+++ modules/vmail/edit.php
@@ -28,6 +28,7 @@ else {
 $is_forward = ($account['type'] == 'forward');
 $is_mailbox = ( ! $is_forward);
 
+/*
 output("<script type=\"text/javascript\">
   
   function selectForwarding() {
@@ -63,13 +64,14 @@ output("<script type=\"text/javascript\">
     }
 
   </script>");
+*/
 
 $form = "
     <p><strong>E-Mail-Adresse:</strong>&#160;<input type=\"text\" name=\"local\" id=\"local\" size=\"10\" value=\"{$account['local']}\" /><strong style=\"font-size: 1.5em;\">&#160;@&#160;</strong>".domainselect($account['domainid'])."</p>";
 
-$form .= "<p><input type=\"checkbox\" id=\"spamfilter\" name=\"spamfilter\" value=\"1\" ".($account['spamfilter'] != NULL ? 'checked="checked" ' : '')." onclick=\"toggleSpamfilterOptions()\" /><label for=\"spamfilter\">&#160;Spam-Filter</label></p>";
+$form .= "<p><input type=\"checkbox\" id=\"spamfilter\" name=\"spamfilter\" value=\"1\" ".($account['spamfilter'] != NULL ? 'checked="checked" ' : '')." /><label for=\"spamfilter\">&#160;Spam-Filter</label></p>";
 
-$form .= "<p style=\"margin-left: 2em; ".($account['spamfilter'] != NULL ? '' : 'display: none;')."\" id=\"spamfilter_options\">
+$form .= "<p style=\"margin-left: 2em;\" id=\"spamfilter_options\">
   <em>Was soll mit E-Mails geschehen, die als Spam eingestuft wurden?</em><br />
   <input type=\"radio\" id=\"spamfilter_folder\" name=\"spamfilter_action\" value=\"folder\" ".($account['spamfilter'] == 'folder' ? 'checked="checked" ' : '')."/><label for=\"spamfilter_folder\">&#160;In IMAP-Unterordner »Spam« ablegen</label><br />
   <input type=\"radio\" id=\"spamfilter_tag\" name=\"spamfilter_action\" value=\"tag\" ".($account['spamfilter'] == 'tag' ? 'checked="checked" ' : '')."/><label for=\"spamfilter_tag\">&#160;Markieren und ganz normal zustellen</label><br />
@@ -78,9 +80,9 @@ $form .= "<p style=\"margin-left: 2em; ".($account['spamfilter'] != NULL ? '' : 
   ";
 
   
-$form .= "<p><input type=\"checkbox\" id=\"virusfilter\" name=\"virusfilter\" value=\"1\" ".($account['virusfilter'] != NULL ? 'checked="checked" ' : '')." onclick=\"toggleVirusfilterOptions()\" /><label for=\"virusfilter\">&#160;Viren-Scanner</label></p>";
+$form .= "<p><input type=\"checkbox\" id=\"virusfilter\" name=\"virusfilter\" value=\"1\" ".($account['virusfilter'] != NULL ? 'checked="checked" ' : '')." /><label for=\"virusfilter\">&#160;Viren-Scanner</label></p>";
 
-$form .= "<p style=\"margin-left: 2em; ".($account['virusfilter'] != NULL ? '' : 'display: none;')."\" id=\"virusfilter_options\">
+$form .= "<p style=\"margin-left: 2em;\" id=\"virusfilter_options\">
   <em>Was soll mit E-Mails geschehen, in denen ein Virus erkannt wurde?</em><br />
   <input type=\"radio\" id=\"virusfilter_folder\" name=\"virusfilter_action\" value=\"folder\" ".($account['virusfilter'] == 'folder' ? 'checked="checked" ' : '')."/><label for=\"virusfilter_folder\">&#160;In IMAP-Unterordner »Viren« ablegen</label><br />
   <input type=\"radio\" id=\"virusfilter_tag\" name=\"virusfilter_action\" value=\"tag\" ".($account['virusfilter'] == 'tag' ? 'checked="checked" ' : '')."/><label for=\"virusfilter_tag\">&#160;Markieren und ganz normal zustellen</label><br />
@@ -94,42 +96,11 @@ if ($is_mailbox and ($account['data'] != ''))
   
 
 $form .= "<p>
-    <input type=\"radio\" id=\"forward\" name=\"type\" value=\"forward\" ".($is_forward ? 'checked="checked" ' : '')." onclick=\"selectForwarding()\" /><label for=\"forward\">&#160;Weiterleitung an andere E-Mail-Adresse</label></p>
-    <p style=\"margin-left: 2em; ".($is_mailbox ? 'display: none' : '')."\" id=\"forward_options\">Weiterleitung an:&#160;<textarea id=\"forward_to\" name=\"forward_to\">".($is_forward ? str_replace(' ', "\n", $account['data']) : '')."</textarea><br />Sie können mehrere Adressen eingeben. trennen Sie diese bitte mit Zeilenumbrüchen oder Leerzeichen.</p>
-    <p><input type=\"radio\" id=\"mailbox\" name=\"type\" value=\"mailbox\" ".($is_mailbox ? 'checked="checked" ' : '')." onclick=\"selectMailbox()\" /><label for=\"mailbox\">&#160;In Mailbox speichern</label></p>
-    <p style=\"margin-left: 2em; ".($is_forward ? 'display: none' : '')."\" id=\"mailbox_options\">Passwort für Abruf:&#160;<input type=\"password\" id=\"password\" name=\"password\" value=\"\" />{$password_message}</p>";
+    <input type=\"radio\" id=\"forward\" name=\"type\" value=\"forward\" ".($is_forward ? 'checked="checked" ' : '')." /><label for=\"forward\">&#160;Weiterleitung an andere E-Mail-Adresse</label></p>
+    <p style=\"margin-left: 2em;\" id=\"forward_options\">Weiterleitung an:<br /><textarea id=\"forward_to\" name=\"forward_to\" rows=\"3\">".($is_forward ? str_replace(' ', "\n", $account['data']) : '')."</textarea><br />Sie können mehrere Adressen eingeben, geben Sie dann bitte eine Adresse pro Zeile ein.</p>
+    <p><input type=\"radio\" id=\"mailbox\" name=\"type\" value=\"mailbox\" ".($is_mailbox ? 'checked="checked" ' : '')." /><label for=\"mailbox\">&#160;In Mailbox speichern</label></p>
+    <p style=\"margin-left: 2em;\" id=\"mailbox_options\">Passwort für Abruf:&#160;<input type=\"password\" id=\"password\" name=\"password\" value=\"\" />{$password_message}</p>";
     
-/*
-$form .= "<tr><td>PHP</td>
-    <td><select name=\"php\" id=\"php\">
-      <option value=\"none\" ".($vhost['php'] == NULL ? 'selected="selected"' : '')." >kein PHP</option>
-      <option value=\"mod_php\" ".($vhost['php'] == 'mod_php' ? 'selected="selected"' : '')." >als Apache-Modul</option>
-      <option value=\"fastcgi\" ".($vhost['php'] == 'fastcgi' ? 'selected="selected"' : '')." >FastCGI</option>
-    </select>
-    </td>
-    <td id=\"defaultphp\">als Apache-Modul</td></tr>
-    <tr><td>SSL-Verschlüsselung</td>
-    <td><select name=\"ssl\" id=\"ssl\">
-      <option value=\"none\" ".($vhost['ssl'] == NULL ? 'selected="selected"' : '')." >SSL optional anbieten</option>
-      <option value=\"http\" ".($vhost['ssl'] == 'http' ? 'selected="selected"' : '')." >kein SSL</option>
-      <option value=\"https\" ".($vhost['ssl'] == 'https' ? 'selected="selected"' : '')." >nur SSL</option>
-      <option value=\"forward\" ".($vhost['ssl'] == 'forward' ? 'selected="selected"' : '')." >Immer auf SSL umleiten</option>
-    </select>
-    </td>
-    <td id=\"defaultssl\">SSL optional anbieten</td></tr>
-    <tr>
-      <td>Logfiles <span class=\"warning\">*</span></td>
-      <td><select name=\"logtype\" id=\"logtype\">
-      <option value=\"none\" ".($vhost['logtype'] == NULL ? 'selected="selected"' : '')." >keine Logfiles</option>
-      <option value=\"anonymous\" ".($vhost['logtype'] == 'anonymous' ? 'selected="selected"' : '')." >anonymisiert</option>
-      <option value=\"default\" ".($vhost['logtype'] == 'default' ? 'selected="selected"' : '')." >vollständige Logfile</option>
-    </select><br />
-    <input type=\"checkbox\" id=\"errorlog\" name=\"errorlog\" value=\"1\" ".($vhost['errorlog'] == 1 ? ' checked="checked" ' : '')." />&#160;<label for=\"errorlog\">Fehlerprotokoll (error_log) einschalten</label>
-    </td>
-    <td id=\"defaultlogtype\">keine Logfiles</td></tr>
-    ";
-
-*/
 $form .= '
   <p><input type="submit" value="Speichern" />&#160;&#160;&#160;&#160;'.internal_link('accounts.php', 'Abbrechen').'</p>';
 
