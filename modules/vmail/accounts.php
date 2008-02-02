753e0f29e561f2c1ba0464efee0e2ec7fa5b103f
bernd <bernd@87cf0b9e-d624-0410-a070-f6ee81989793>
2008-02-02 15:07:32 +0000

    Hinweis für vmail-user
    git-svn-id: https://svn.schokokeks.org/repos/tools/webinterface/trunk@935 87cf0b9e-d624-0410-a070-f6ee81989793

--- modules/vmail/accounts.php
+++ modules/vmail/accounts.php
@@ -21,9 +21,10 @@ output('<h3>E-Mail-Accounts</h3>
             <td><a href="save.php?action=delete&amp;id='.$account['id'].'">löschen</a></td></tr>');
         }
         output('</table>
-<p><a href="edit.php">Neuen Account anlegen</a></p>
+<p><a href="edit.php">Neuen Account anlegen</a></p>');
 
-');
+/* FIXME: Das sollte nur kommen, wenn der IMAP/POP3-Menü-Eintrag nicht da ist */
+output('<p>Hinweis: schokokeks.org bietet für fortgeschrittene Nutzer die manuelle Einrichtung von POP3/IMAP-Accounts.<br/><a href="'.$prefix.'go/imap/accounts.php?action=create">Neuen POP3/IMAP-Account anlegen</a></p>');
 
 
 
