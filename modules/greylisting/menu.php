4d046e432ac0c094098879f934285ac2085db342
bernd <bernd@87cf0b9e-d624-0410-a070-f6ee81989793>
2008-03-11 10:12:19 +0000

    Menü-Reihenfolge logischer
    git-svn-id: https://svn.schokokeks.org/repos/tools/webinterface/trunk@986 87cf0b9e-d624-0410-a070-f6ee81989793

--- modules/greylisting/menu.php
+++ modules/greylisting/menu.php
@@ -4,7 +4,7 @@ $role = $_SESSION['role'];
 
 if ($role & ROLE_SYSTEMUSER)
 {
-  $menu['greylisting_whitelist'] = array("label" => "Greylisting", "file" => "whitelist.php", "weight" => 16);
+  $menu['greylisting_whitelist'] = array("label" => "Greylisting", "file" => "whitelist.php", "weight" => 5);
 }
 
 ?>
