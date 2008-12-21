3a2d8389e24ad1a5670451b43c7f44845f311bd4
bernd <bernd@87cf0b9e-d624-0410-a070-f6ee81989793>
2008-12-21 07:54:55 +0000

    Zeige Freewvs-Resultate an
    git-svn-id: https://svn.schokokeks.org/repos/tools/webinterface/trunk@1201 87cf0b9e-d624-0410-a070-f6ee81989793

--- modules/freewvs/freewvs.php
+++ modules/freewvs/freewvs.php
@@ -1,30 +1,59 @@
 <?php
 
 require_once('session/start.php');
+
+require_once('freewvs.php');
+
 require_role(array(ROLE_SYSTEMUSER));
 
 $uid = (int) $_SESSION['userinfo']['uid'];
 
 if (in_array($_POST['freq'],array("day","week","month"))) {
+  check_form_token('freewvs_freq'); 
 	db_query("REPLACE INTO qatools.freewvs (user,freq) VALUES ({$uid},'{$_POST['freq']}');");
 	header("Location: freewvs");
 	die();
 }
 
-output('<h3>Überprüfung Ihrer Web-Anwendungen auf Sicherheitslücken</h3>');
-
 $result = db_query("SELECT freq FROM qatools.v_freewvs WHERE uid={$uid};");
 $result=mysql_fetch_assoc($result);
 $freq=$result['freq'];
 
-output('<p>Mit dem Programm freewvs kann automatisiert geprüft werden, ob Ihre Web-Anwendungen (z.B. Blog-Software, Content-Management-Systeme, ...) noch aktuell sind oder ob es in den von Ihnen verwendeten Versionen Sicherheitslücken gibt.</p>
-<p>Diese Option ermöglicht Ihnen, vollautomatisch regelmäßige Prüfungen Ihrer Webanwendungen mit Hilfe von freewvs durchzuführen. Sollten Probleme festgestellt werden, erhalten Sie Informationen darüber per E-Mail.</p>');
-$form='
-<div style="margin-left: 2em;">
-  <p><input id="day" type="radio" name="freq" value="day" '.($freq=="day"?'checked="checked" ':"").'/><label for="day">täglich</label></p>
-  <p><input id="week" type="radio" name="freq" value="week" '.($freq=="week"?'checked="checked" ':"").'/><label for="week">wöchentlich</label></p>
-  <p><input id="month" type="radio" name="freq" value="month" '.($freq=="month"?'checked="checked" ':"").'/><label for="month">monatlich</label></p>
-  <p><input type="submit" value="Speichern"/></p>
-</div>';
+output('<h3>Überprüfung Ihrer Web-Anwendungen auf Sicherheitslücken</h3>');
 
-output(html_form('freewvs_freq','','',$form));
+output('<p>Das Programm freewvs prüft automatisch regelmäßig Ihre Web-Anwendungen (z.B. Blog-Software, Content-Management-Systeme, ...) auf bekannte Sicherheitsprobleme. Sie können festlegen, wie oft Sie bei gefundenen Problemen benachrichtigt werden möchten.</p>
+<p><strong>Wie oft möchten Sie über Sicherheitsprobleme benachrichtigt werden?</strong></p>
+'.html_form('freewvs_freq', '', '', '<p>'.html_select('freq', array('day' => 'täglich', 'week' => 'höchstens einmal pro Woche', 'month' => 'höchstens einmal pro Monat'), $freq).' &#160; <input type="submit" value="speichern" />').'</p>');
+
+$results = load_results();
+
+output('<h3>Aktuell installierte Web-Anwendungen</h3>
+<p>Die folgenden Web-Anwendungen wurden beim letzten Programmdurchlauf gefunden. Diese Liste wird i.d.R. täglich aktualisiert.</p>');
+foreach ($results AS $app) {
+  output("<div class='freewvs freewvs-{$app['state']}'>\n");
+  if ($app['state'] == 'ok') {
+    output("<img src='{$prefix}images/ok.png' />\n");
+    output("<p><strong>{$app['appname']} {$app['version']}</strong></p>\n");
+    output("<p>Diese Anwendung ist aktuell und hat keine allgemein bekannten Sicherheitsprobleme.</p>\n");
+    output("<p>Gefunden in {$app['directory']}</p>\n");
+  }
+  else {
+    $vulnlink = $app['vulninfo'];
+    if (substr($vulnlink, 0, 3) == 'CVE') {
+      $vulnlink = 'http://cve.mitre.org/cgi-bin/cvename.cgi?name='.$vulnlink;
+    }
+    output("<img src='{$prefix}images/error.png' />\n");
+    output("<p><strong>{$app['appname']} {$app['version']}</strong></p>\n");
+    if ($app['safeversion'] != '') {
+      output("<p>Diese Anwendung ist von Sicherheits-Problemen betroffen. Ein <strong>Update auf Version {$app['safeversion']}</strong> wird dringend empfohlen. Prüfen Sie anhand der unten genannten Referenz welche Gefahren von dieser Anwendung momentan ausgehen.</p>\n");
+    } else {
+      output("<p>Diese Anwendung ist von Sicherheits-Problemen betroffen. Leider gibt es <strong>momentan keine aktualisierte Version</strong>. Prüfen Sie bitte anhand der unten genannten Beschreibung des Problem die möglichen Gefahren eines weiteren Betriebs dieser Anwendung.</p>\n");
+    }
+    output("<p><strong>Referenz zu diesem Sicherheitsproblem: <a href='{$vulnlink}'>{$app['vulninfo']}</a></strong></p>");
+    output("<p>Gefunden in {$app['directory']}</p>\n");
+  }
+  output("</div>\n");
+  #output("<tr><td>{$app['appname']} ({$app['version']})</td><td>{$app['state']}</td></tr>");
+}
+#output('</table>');
+
