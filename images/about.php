33e9944cae87f4eacebdf458f6cd3894a8630940
bernd <bernd@87cf0b9e-d624-0410-a070-f6ee81989793>
2009-02-11 17:33:39 +0000

    Bilder-Lizenz-Infos
    git-svn-id: https://svn.schokokeks.org/repos/tools/webinterface/trunk@1243 87cf0b9e-d624-0410-a070-f6ee81989793

---
+++ images/about.php
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de">
+<head>
+
+<title>Administration - Bildrechte</title>
+<link rel="stylesheet" href="../css/admin.css" type="text/css" media="screen" title="Normal" /></head>
+<body>
+
+<div class="content">
+<a name="content" style="display: none"> </a>
+
+
+<h3>Lizenzinformationen</h3>
+<p>Dieses Webinterface ist eine Eigenentwicklung von schokokeks.org Webhosting.</p>
+<p>Einzelne Grafiken wurden aus freien Projekten entnommen oder es wurden freie Grafiken verwendet.</p>
+<p>Der Ursprung von Grafiken ist:</p>
+<ul>
+  <li><a href="http://www.famfamfam.com/lab/icons/silk/">Das »Silk«-Iconset von Mark James</a>, lizensiert unter <a href="http://creativecommons.org/licenses/by/2.5/">CC-by 2.5</a></li>
+  <li>Icons vom <a href="http://www.phpmyadmin.net/">phpMyAdmin-Projekt</a>, lizensiert unter <a href="http://www.gnu.org/licenses/gpl-2.0.html">GNU GPL 2.0</a></li>
+  <li>Das Logo der <a href="http://www.jabber.org/">Jabber Software Foundation</a>, lizensiert unter <a href="http://creativecommons.org/licenses/by/2.5/">CC-by 2.5</a></li>
+</ul>
+
+<p>Diese Urheberinformation ist nach bestem Wissen erstellt. Sollten Sie Fehler erkennen oder verwenden wir Ihre Arbeit ohne dies hier zu erwähnen, dann <a href="http://www.schokokeks.org/kontakt">kontaktieren Sie uns</a> bitte.</p>
+
+</div>
+
+</body>
+</html>
+
--- inc/bottom.php
+++ inc/bottom.php
@@ -4,11 +4,11 @@ if (! defined ("BOTTOM_INCLUDED"))
 {
 define("BOTTOM_INCLUDED", true);
 ?>
+</div>
 <div class="foot">
-<p>Sollten Sie auf dieser Administrations-Oberfläche ein Problem entdecken oder Hilfe benötigen, schreiben Sie bitte eine einfache eMail an <a href="mailto:root@schokokeks.org">root@schokokeks.org</a>. Unser <a href="http://www.schokokeks.org/kontakt">Impressum</a> finden Sie auf der <a href="http://www.schokokeks.org/">öffentlichen Seite</a>.</p>
+<p>Sollten Sie auf dieser Administrations-Oberfläche ein Problem entdecken oder Hilfe benötigen, schreiben Sie bitte eine einfache eMail an <a href="mailto:root@schokokeks.org">root@schokokeks.org</a>. Unser <a href="http://www.schokokeks.org/kontakt">Impressum</a> finden Sie auf der <a href="http://www.schokokeks.org/">öffentlichen Seite</a>. Lizenzinformationen zu diesem Webinterface und verwendeten Rechten finden Sie <a href="<?php echo $prefix; ?>images/about.php">indem Sie hier klicken</a>.</p>
 </div>
 
-</div>
 
 <?php
 /*<div id="beta">
