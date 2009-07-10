678cb1aa17317b1d50e1fcbeeb20f9a44ec4e4cb
bernd <bernd@87cf0b9e-d624-0410-a070-f6ee81989793>
2009-07-10 07:00:01 +0000

    Fehlende Datei dazu gefügt
    git-svn-id: https://svn.schokokeks.org/repos/tools/webinterface/trunk@1406 87cf0b9e-d624-0410-a070-f6ee81989793

---
+++ modules/webapps/waitforupdate.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once('inc/base.php');
+
+require_role(ROLE_SYSTEMUSER);
+
+$title='Update wird ausgeführt';
+$section = 'webapps_freewvs';
+
+output('<h3>Update wird ausgeführt</h3>');
+
+output('<p>Sie haben ein Update Ihrer Web-Anwendung in Autrag gegeben. Dieses Update wird in Kürze automatisiert ausgeführt.
+Sie erhalten dazu eine E-Mail-Bestätigung über den Erfolg oder Misserfolg des automatischen Updates. <strong>Dies dauert bis zu 15 Minuten!</strong></p>
+
+<p>Für den Fall einer Fehlfunktion des autoamtischen Updates werden vorab Sicherheitskopien Ihrer alten Dateien und der verwendeten Datenbanken erzeugt. Diese finden Sie unter <strong>~/backup/</strong> in Ihrem Home-Verzeichnis. Wenn die neue Version einige Zeit ohne Beanstandungen funktioniert, sollten Sie die alten Backups löschen um Speicherplatz zu sparen.</p>');
+
+
+
