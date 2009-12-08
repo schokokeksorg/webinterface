fc969c9c73b5d74c721297943ff557d96e682378
bernd <bernd@87cf0b9e-d624-0410-a070-f6ee81989793>
2009-12-08 15:46:56 +0000

    FTP-Benutzer => FTP-Zugriff
    git-svn-id: https://svn.schokokeks.org/repos/tools/webinterface/trunk@1536 87cf0b9e-d624-0410-a070-f6ee81989793

--- modules/ftpusers/menu.php
+++ modules/ftpusers/menu.php
@@ -4,7 +4,7 @@ $role = $_SESSION['role'];
 
 if ($role & ROLE_SYSTEMUSER)
 {
-  $menu["ftpusers_accounts"] = array("label" => "FTP-Benutzer", "file" => "accounts", "weight" => 35);
+  $menu["ftpusers_accounts"] = array("label" => "FTP-Zugriff", "file" => "accounts", "weight" => 35);
 }
 
 ?>
