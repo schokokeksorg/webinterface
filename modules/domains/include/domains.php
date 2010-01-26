9afc4f3e88eba11c38cb8921e7d6cd5b2bf34ba3
bernd <bernd@87cf0b9e-d624-0410-a070-f6ee81989793>
2010-01-26 15:48:59 +0000

    Mailman-Domains nur auslesen wenn Mailman-Modul aktiviert ist
    git-svn-id: https://svn.schokokeks.org/repos/tools/webinterface/trunk@1631 87cf0b9e-d624-0410-a070-f6ee81989793

--- modules/domains/include/domains.php
+++ modules/domains/include/domains.php
@@ -3,8 +3,13 @@
 require_once('inc/base.php');
 require_once('inc/debug.php');
 
+
 function mailman_subdomains($domain)
 {
+  if ( ! in_array('mailman', config('modules')))
+  {
+    return array();
+  }
   $domain = (int) $domain;
   $result = db_query("SELECT id, hostname FROM mail.mailman_domains WHERE domain={$domain}");
   $ret = array();
