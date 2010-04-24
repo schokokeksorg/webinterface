1895bc2fdfa4f63682c4ae84851fca4b7cda23e3
bernd <bernd@87cf0b9e-d624-0410-a070-f6ee81989793>
2010-04-24 09:23:37 +0000

    SHA-512 auch für E-mail
    git-svn-id: https://svn.schokokeks.org/repos/tools/webinterface/trunk@1727 87cf0b9e-d624-0410-a070-f6ee81989793

--- modules/email/include/common.php
+++ modules/email/include/common.php
@@ -4,10 +4,20 @@ function encrypt_mail_password($pw)
 {
   DEBUG("unencrypted PW: ".$pw);
   require_once('inc/base.php');
-  $salt = random_string(8);
-  $encpw = crypt($pw, "\$1\${$salt}\$");
-  DEBUG("encrypted PW: ".$encpw);
-  return chop($encpw);
+  $newpass = '';
+  if (defined("CRYPT_SHA512") && CRYPT_SHA512 == 1)
+  {
+    $rounds = rand(1000, 5000);
+    $salt = "rounds=".$rounds."$".random_string(8);
+    $newpass = crypt($newpass, "\$6\${$salt}\$");
+  }
+  else
+  {
+    $salt = random_string(8);
+    $newpass = crypt($newpass, "\$1\${$salt}\$");
+  }
+  DEBUG("encrypted PW: ".$newpass);
+  return chop($newpass);
 
 }
 
