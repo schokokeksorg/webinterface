c4e50a25b31d5b5cfadcd32e859da007756d2364
bernd <bernd@87cf0b9e-d624-0410-a070-f6ee81989793>
2010-04-29 06:30:29 +0000

    Passwort wurde nicht übernommen
    git-svn-id: https://svn.schokokeks.org/repos/tools/webinterface/trunk@1729 87cf0b9e-d624-0410-a070-f6ee81989793

--- modules/email/include/common.php
+++ modules/email/include/common.php
@@ -1,19 +1,20 @@
 <?php
 
-function encrypt_mail_password($pw)
+function encrypt_mail_password($newpass)
 {
-  DEBUG("unencrypted PW: ".$pw);
+  DEBUG("unencrypted PW: »".$newpass."«");
   require_once('inc/base.php');
-  $newpass = '';
   if (defined("CRYPT_SHA512") && CRYPT_SHA512 == 1)
   {
     $rounds = rand(1000, 5000);
     $salt = "rounds=".$rounds."$".random_string(8);
+    DEBUG("crypt(\"{$newpass}\", \"\$6\${$salt}\$\");");
     $newpass = crypt($newpass, "\$6\${$salt}\$");
   }
   else
   {
     $salt = random_string(8);
+    DEBUG("crypt(\"{$newpass}\", \"\$1\${$salt}\$\");");
     $newpass = crypt($newpass, "\$1\${$salt}\$");
   }
   DEBUG("encrypted PW: ".$newpass);
