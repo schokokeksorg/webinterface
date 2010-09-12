fab82227aa6878e4945b2392ee93ec66e775e0aa
bernd <bernd@87cf0b9e-d624-0410-a070-f6ee81989793>
2010-09-12 05:22:40 +0000

    Menü-Einträge nicht gleich gewichten
    git-svn-id: https://svn.schokokeks.org/repos/tools/webinterface/trunk@1809 87cf0b9e-d624-0410-a070-f6ee81989793

--- modules/index/menu.php
+++ modules/index/menu.php
@@ -4,10 +4,10 @@ $role = $_SESSION['role'];
 
 if ($role == ROLE_ANONYMOUS) {
   $menu["index_login"] = array("label" => "Login", "file" => "index", "weight" => 0);
-  $menu["certlogin"] = array("label" => "Client-Zertifikat", "file" => "certinfo", "weight" => 0);
+  $menu["certlogin"] = array("label" => "Client-Zertifikat", "file" => "certinfo", "weight" => 10);
 } else {
   if ($role & ROLE_SYSTEMUSER)
-    $menu["index_cert"] = array("label" => "Client-Zertifikat", "file" => "cert", "weight" => 0, "submenu" => "index_index");
+    $menu["index_cert"] = array("label" => "Client-Zertifikat", "file" => "cert", "weight" => 10, "submenu" => "index_index");
   if ($role & (ROLE_SYSTEMUSER | ROLE_CUSTOMER)) {
     $menu["index_chpass"] = array("label" => "Passwort ändern", "file" => "chpass", "weight" => 98);
   }
