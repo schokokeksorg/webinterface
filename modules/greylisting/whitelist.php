8b124c0bc0535c9e063fb24ff168b1c317b96f27
bernd <bernd@87cf0b9e-d624-0410-a070-f6ee81989793>
2011-03-22 04:52:53 +0000

    UI-Verbesserungen Greylisting
    git-svn-id: https://svn.schokokeks.org/repos/tools/webinterface/trunk@1959 87cf0b9e-d624-0410-a070-f6ee81989793

--- modules/greylisting/whitelist.php
+++ modules/greylisting/whitelist.php
@@ -19,15 +19,15 @@ geringem Aufwand.</p>
 von einem eigentlich legitimen Absender, der an keinen unserer Benutzer bisher
 E-Mails gesendet hat, einige Zeit verspätet zugestellt werden.</p>
 <p>Sofern Sie eine derartige E-Mail erwarten, also z.B. sich auf einer fremden Website
-mit Ihrer E-Mail-Adresse anmelden möchten oder ähnliches, dann können Sie hier Ihre
-dafür benutzte Adresse eintragen. E-Mails an diese Adresse werden dann umgehend zugestellt.</p>
+mit Ihrer E-Mail-Adresse anmelden möchten oder ähnliches, dann können Sie hier <strong>Ihre 
+E-Mail-Adresse</strong> eintragen. E-Mails an diese Adresse werden dann umgehend zugestellt.</p>
 <p>Dabei können Sie Adressen wahlweise nur kurzzeitig oder dauerhaft vom Greylisting ausnehmen. 
 Sie können auch lediglich einen Domainnamen angeben, dann sind sämtliche Adressen innerhalb 
 dieser Domain ausgenommen.</p>
 ");
 
 $form = "<table>
-    <tr><th>Adresse</th><th>seit</th><th>bis</th><th> </th></tr>
+    <tr><th>Empfänger-Adresse</th><th>seit</th><th>bis</th><th> </th></tr>
     ";
 
 foreach ($whitelist AS $entry)
@@ -35,7 +35,11 @@ foreach ($whitelist AS $entry)
 	$end = $entry['expire'];
 	if (! $end)
 		$end = '<em>unbegrenzt</em>';
-	$form .= "<tr><td>{$entry['local']}@{$entry['domain']}</td><td>{$entry['date']}</td><td>{$end}</td><td>".internal_link("save", "<img src=\"{$prefix}images/delete.png\" alt=\"Eintrag löschen\" title=\"Diesen Eintrag löschen\" style=\"width: 16px; height: 16px;\" />", "action=delete&id={$entry['id']}")."</td></tr>\n";
+  $local = $entry['local'];
+  if (empty($local)) {
+    $local = '*';
+  }
+	$form .= "<tr><td>{$local}@{$entry['domain']}</td><td>{$entry['date']}</td><td>{$end}</td><td>".internal_link("save", "<img src=\"{$prefix}images/delete.png\" alt=\"Eintrag löschen\" title=\"Diesen Eintrag löschen\" style=\"width: 16px; height: 16px;\" />", "action=delete&id={$entry['id']}")."</td></tr>\n";
 }
 
 $form .= '<tr><td><input type="text" name="address" /></td><td>-</td><td>'.html_select('expire', array('none' => 'Unbegrenzt', '30' => '30 Minuten', '60' => '1 Stunde', '720' => '12 Stunden', '1440' => '1 Tag', '2880' => '2 Tage', '10080' => '1 Woche', '43200' => '30 Tage'), '1440').'</td><td></td></tr>';
