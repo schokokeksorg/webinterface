ff7fa1053bbc1990a551bef1c1d8e123b9efbe13
bernd <bernd@87cf0b9e-d624-0410-a070-f6ee81989793>
2011-07-27 07:51:18 +0000

    Username-Feld beim Login fokussieren
    git-svn-id: https://svn.schokokeks.org/repos/tools/webinterface/trunk@2034 87cf0b9e-d624-0410-a070-f6ee81989793

--- themes/default/page-login.tpl.php
+++ themes/default/page-login.tpl.php
@@ -17,7 +17,7 @@ else
 <?php echo $html_header; ?>
 </head>
 
-<body>
+<body onload="javascript:document.getElementById('username').focus();">
 <div><a href="#content" style="display: none;">Zum Inhalt</a></div>
 
 <div class="menu">
@@ -41,8 +41,8 @@ if ($messages) {
 <h3>schokokeks.org Hosting Webinterface</h3>
 <p>Auf dieser Seite können Sie diverse Einstellungen Ihres Accounts auf schokokeks.org Hosting festlegen. Sofern Sie noch kein Kunde von schokokeks.org Hosting sind, können Sie diese Seite nicht benutzen. Besuchen Sie in diesem Fall bitte unsere <a href="http://www.schokokeks.org">öffentliche Seite</a>.</p>
 <form action="" method="post">
-<p><span class="login_label">Benutzer<sup>*</sup>:</span> <input type="text" name="username" size="30" /></p>
-<p><span class="login_label">Passwort:</span> <input type="password" name="password" size="30" /></p>
+<p><label for="username" class="login_label">Benutzer<sup>*</sup>:</label> <input type="text" id="username" name="username" size="30" /></p>
+<p><label for="password" class="login_label">Passwort:</label> <input type="password" id="password" name="password" size="30" /></p>
 <p><span class="login_label">&#160;</span> <input type="submit" value="Anmelden" /></p>
 </form>
 <p><sup>*</sup> Sie können sich hier mit Ihrem System-Benutzernamen, Ihrem IMAP-Account oder Ihrer Kundennummer (jeweils mit zugehörigem Passwort) anmelden. Je nach gewählten Daten erhalten Sie unterschiedliche Zugriffsrechte.</p>
