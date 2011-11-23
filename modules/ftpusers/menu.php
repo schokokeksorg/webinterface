c437df0ee68454648bc275fca3ae7fd26109b295
bernd <bernd@87cf0b9e-d624-0410-a070-f6ee81989793>
2011-11-23 10:04:11 +0000

    FTP-Zugänge als Unterpunkt von System-Users
    git-svn-id: https://svn.schokokeks.org/repos/tools/webinterface/trunk@2070 87cf0b9e-d624-0410-a070-f6ee81989793

--- modules/ftpusers/menu.php
+++ modules/ftpusers/menu.php
@@ -4,7 +4,11 @@ $role = $_SESSION['role'];
 
 if ($role & ROLE_SYSTEMUSER)
 {
+  if (have_module('systemuser')) {
+    $menu["ftpusers_accounts"] = array("label" => "FTP-Zugriff", "file" => "accounts", "weight" => 35, 'submenu' => 'systemuser_account');
+  } else {
   $menu["ftpusers_accounts"] = array("label" => "FTP-Zugriff", "file" => "accounts", "weight" => 35);
+  }
 }
 
 ?>
