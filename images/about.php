c208bd906b3991555db11b9229846c4601ca408c
Bernd Wurst <bernd@schokokeks.org>
2012-03-11 15:40:04 +0100

    Added license tags for CC0, README and COPYING

--- images/about.php
+++ images/about.php
@@ -1,4 +1,26 @@
 <?xml version="1.0" encoding="utf-8"?>
+<!-- 
+This file belongs to the Webinterface of schokokeks.org Hosting
+
+Written 2008-2012 by schokokeks.org Hosting, namely
+  Bernd Wurst <bernd@schokokeks.org>
+  Hanno Böck <hanno@schokokeks.org>
+
+To the extent possible under law, the author(s) have dedicated 
+all copyright and related and neighboring rights to this 
+software to the public domain worldwide. This software is 
+distributed without any warranty.
+
+You should have received a copy of the CC0 Public Domain 
+Dedication along with this software. If not, see 
+http://creativecommons.org/publicdomain/zero/1.0/
+
+Nevertheless, in case you use a significant part of this 
+code, we ask (but not require, see the license) that you 
+keep the authors' names in place and return your changes 
+to the public. We would be especially happy if you tell 
+us what you're going to do with this code.
+-->
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
     "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 
@@ -14,7 +36,7 @@
 
 
 <h3>Lizenzinformationen</h3>
-<p>Dieses Webinterface ist eine Eigenentwicklung von schokokeks.org Webhosting.</p>
+<p>Dieses Webinterface ist eine Eigenentwicklung von schokokeks.org Webhosting. Der Source-Code ist unter <a href="http://source.schokokeks.org/">source.schokokeks.org</a> erhältlich und wurde unter der Lizenz CC0 / Public Domain freigegeben.</p>
 <p>Einzelne Grafiken wurden aus freien Projekten entnommen oder es wurden freie Grafiken verwendet.</p>
 <p>Der Ursprung von Grafiken ist:</p>
 <ul>
--- modules/register/include/mail.php
+++ modules/register/include/mail.php
@@ -1,4 +1,19 @@
 <?php
+/*
+This file belongs to the Webinterface of schokokeks.org Hosting
+
+Written 2008-2012 by schokokeks.org Hosting, namely
+  Bernd Wurst <bernd@schokokeks.org>
+  Hanno Böck <hanno@schokokeks.org>
+
+To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+
+You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see 
+http://creativecommons.org/publicdomain/zero/1.0/
+
+Nevertheless, in case you use a significant part of this code, we ask (but not require, see the license) that you keep the authors' names in place and return your changes to the public. We would be especially happy if you tell us what you're going to do with this code.
+*/
+
 require_once('newpass.php');
 require_once('session/checkuser.php');
 
