f12aba6daa6e2848a8ed60ea57b26874d6675f52
Bernd Wurst <bernd@schokokeks.org>
2013-01-19 10:49:50 +0100

    Updated copyright notice (2012 => 2013)

--- dispatch.php
+++ dispatch.php
@@ -3,7 +3,7 @@
 This file belongs to the Webinterface of schokokeks.org Hosting.
 Please see http://source.schokokeks.org for the newest source files.
 
-Written 2008-2012 by schokokeks.org Hosting, namely
+Written 2008-2013 by schokokeks.org Hosting, namely
   Bernd Wurst <bernd@schokokeks.org>
   Hanno Böck <hanno@schokokeks.org>
 
