4ba2a6d0233e8a3fbf5c0eadcb6f48e02644ff1f
Bernd Wurst <bernd@schokokeks.org>
2013-11-12 18:26:44 +0100

    Logikfehler: Subuser hatte immer alle Recht. Nicht gut.

--- session/checkuser.php
+++ session/checkuser.php
@@ -296,6 +296,7 @@ function setup_session($role, $useridentity)
     DEBUG("We are a sub-user");
     $info = get_subuser_info($useridentity);
     $_SESSION['userinfo'] = $info;
+    $_SESSION['restrict_modules'] = explode(',', $info['modules']);
     $_SESSION['role'] = ROLE_SYSTEMUSER | ROLE_SUBUSER;
     $_SESSION['subuser'] = $useridentity;
     $data = db_query("SELECT kundenaccount FROM system.useraccounts WHERE username='{$info['username']}'");
@@ -304,7 +305,6 @@ function setup_session($role, $useridentity)
         $customer = get_customer_info($_SESSION['userinfo']['username']);
         $_SESSION['customerinfo'] = $customer;
         $_SESSION['role'] = ROLE_SYSTEMUSER | ROLE_CUSTOMER | ROLE_SUBUSER;
-        $_SESSION['restrict_modules'] = explode(',', $info['modules']);
       }
     }
     logger(LOG_INFO, "session/start", "login", "logged in user »{$info['username']}«");
