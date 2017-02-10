bbc3bcac38ffcf87fb8335c24737c562ccabc458
Bernd Wurst <bernd@schokokeks.org>
2017-02-10 17:09:35 +0100

    Hostnames dürfen keine Unterstriche enthalten (außer als jeweils erstes Zeichen)

--- inc/security.php
+++ inc/security.php
@@ -72,6 +72,9 @@ function filter_input_hostname( $input, $wildcard=false )
   $input = str_replace(array('Ä', 'Ö', 'Ü'), array('ä', 'ö', 'ü'), strtolower($input));
   $input = rtrim($input, "\t\n\r\x00 .");
   $input = ltrim($input, "\t\n\r\x00 .");
+  if (preg_replace("/[^.]_/", "", $input) != $input) {
+    system_failure("Der Unterstrich ist nur als erstes Zeichen eines Hostnames erlaubt.");
+  }
   if (preg_replace("/[^[:alnum:]äöü_*\.\-]/", "", $input ) != $input)
     system_failure("Ihre Daten enthielten ungültige Zeichen!");
   if (preg_match("/^.+\*/", $input ))
