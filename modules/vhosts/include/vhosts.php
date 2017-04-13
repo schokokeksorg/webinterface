0f8485a56a2d2f5957ea78641d8aa2662391b6f5
Hanno Böck <hanno@hboeck.de>
2017-04-13 17:31:08 +0200

    make PHP 7.1 default

--- modules/vhosts/include/vhosts.php
+++ modules/vhosts/include/vhosts.php
@@ -85,7 +85,7 @@ function empty_vhost()
   
   $vhost['homedir'] = $_SESSION['userinfo']['homedir'];
   $vhost['docroot'] = NULL;
-  $vhost['php'] = 'fpm70';
+  $vhost['php'] = 'fpm71';
   $vhost['cgi'] = 1;
   $vhost['ssl'] = NULL;
   $vhost['hsts'] = -1;
