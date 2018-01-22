9609f4ea9c173a22d89c5fdaff6ecdbc03c699fe
Bernd Wurst <bernd@schokokeks.org>
2018-01-22 11:50:11 +0100

    Zeige Domain-Details nur bei Domains die über uns verwaltet werden

--- modules/domains/domains.php
+++ modules/domains/domains.php
@@ -75,7 +75,7 @@ foreach ($user_domains as $domain)
     $punycode = '';
   }
   $domainname = "{$domain->fqdn}{$punycode}";
-  if ($_SESSION['role'] & ROLE_CUSTOMER) {
+  if ($_SESSION['role'] & ROLE_CUSTOMER && $domain->provider=='terions') {
       $domainname = internal_link('detail', $domainname, 'id='.$domain->id);
   }
   output("  <tr><td>{$domainname}</td><td>{$regdate}</td><td>{$features}</td></tr>\n");
