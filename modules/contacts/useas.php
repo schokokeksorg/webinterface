3feaa185aa7f2bc21794b8fb284f56f85f65846e
Bernd Wurst <bernd@schokokeks.org>
2018-01-23 16:17:55 +0100

    Ermögliche das Ändern einer Domain aus der Detailansicht des Handles heraus

--- modules/contacts/useas.php
+++ modules/contacts/useas.php
@@ -101,7 +101,7 @@ if (isset($_REQUEST['useas'])) {
             }
             $funktion = implode(' und ', $funktion);
 
-            output('<p>Ist <strong>'.$funktion.'</strong> bei der Domain <strong>'.$d->fqdn.'</strong>.</p>');
+            output('<p>Ist <strong>'.$funktion.'</strong> bei der Domain <strong>'.$d->fqdn.'</strong>. '.internal_link('../domains/update', icon_edit()." Inhaber dieser Domain ändern", 'id='.$d->id).'</p>');
         }
 
 
