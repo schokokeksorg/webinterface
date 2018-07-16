fc1849c20575350bc8c6e6840f669d720b50df77
Bernd Wurst <bernd@schokokeks.org>
2018-07-16 12:57:24 +0200

    Wandle IBAN in Großbuchstaben

--- modules/invoice/save.php
+++ modules/invoice/save.php
@@ -51,7 +51,7 @@ if ($_GET['action'] == 'new') {
     if (empty($_REQUEST['iban'])) {
         system_failure('Es wurde keine IBAN angegeben.');
     }
-    $iban = str_replace(' ', '', $_REQUEST['iban']);
+    $iban = str_replace(' ', '', strtoupper($_REQUEST['iban']));
     if (! verify_iban($iban)) {
         system_failure("Die IBAN scheint nicht korrekt zu sein!");
     }
--- modules/invoice/sepamandat.js
+++ modules/invoice/sepamandat.js
@@ -16,8 +16,9 @@ function populate_bankinfo(result) {
 
 function searchbank() 
 {
-  var iban = $('#iban').val().replace(/\s/g, '');
+  var iban = $('#iban').val().toUpperCase().replace(/\s/g, '');
   if (iban.substr(0,2) == "DE" && iban.length == 22) {
+    $("#iban").val(iban);
     $("#bankname").prop("disabled", true);
     $("#bic").prop("disabled", true);
     $.getJSON("sepamandat_banksearch?iban="+iban, populate_bankinfo)
