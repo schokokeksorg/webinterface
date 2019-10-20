fa4c5125ff883e836bf87d5c9d3d2e58cdd3cd89
Bernd Wurst <bernd@schokokeks.org>
2019-10-20 12:02:54 +0200

    fix encoding mail headers and body as quoted printable

--- inc/base.php
+++ inc/base.php
@@ -479,7 +479,9 @@ function send_mail($address, $subject, $body)
     if (strstr($subject, "\n") !== false) {
         die("Zeilenumbruch im subject!");
     }
-    $header = "From: ".config('company_name')." Web Administration <noreply@".config('masterdomain').">\r\nReply-To: ".config('adminmail')."\r\nCc: ".config('adminmail')."\r\nContent-Type: text/plain; charset=\"utf-8\"\r\nContent-Transfer-Encoding: 8bit";
+    $header = "From: ".config('company_name')." Web Administration <noreply@".config('masterdomain').">\r\nReply-To: ".config('adminmail')."\r\nCc: ".config('adminmail')."\r\nContent-Type: text/plain; charset=\"utf-8\"\r\nContent-Transfer-Encoding: quoted-printable";
+    $subject = "=?UTF-8?Q?".quoted_printable_encode($subject)."?=";
+    $body = quoted_printable_encode($body);
     mail($address, $subject, $body, $header);
 }
 
