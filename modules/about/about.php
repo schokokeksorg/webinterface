dcc202fb249a446ac15c7cf413b9f1b4a3f31b58
Hanno Böck <hanno@gentoo.org>
2022-08-20 09:22:23 +0200

    Change license from CC0 to 0BSD, all contributors agreed

--- modules/about/about.php
+++ modules/about/about.php
@@ -2,21 +2,18 @@
 /*
 This file belongs to the Webinterface of schokokeks.org Hosting
 
-Written 2008-2018 by schokokeks.org Hosting, namely
+Written by schokokeks.org Hosting, namely
   Bernd Wurst <bernd@schokokeks.org>
   Hanno Böck <hanno@schokokeks.org>
 
-To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
-
-You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see
-https://creativecommons.org/publicdomain/zero/1.0/
+This code is published under a 0BSD license.
 
 Nevertheless, in case you use a significant part of this code, we ask (but not require, see the license) that you keep the authors' names in place and return your changes to the public. We would be especially happy if you tell us what you're going to do with this code.
 */
 
 title('Lizenzinformationen');
 
-output('<p>Dieses Webinterface ist eine Eigenentwicklung von schokokeks.org Webhosting. Der Source-Code ist unter <a href="https://source.schokokeks.org/">source.schokokeks.org</a> erhältlich und wurde unter der Lizenz CC0 / Public Domain freigegeben.</p>
+output('<p>Dieses Webinterface ist eine Eigenentwicklung von schokokeks.org Webhosting. Der Source-Code ist unter <a href="https://source.schokokeks.org/">source.schokokeks.org</a> erhältlich und wurde unter der 0BSD-Lizenz freigegeben.</p>
 <p>Folgende Bestandteile wurden von anderen Autoren übernommen und unterliegen einer abweichenden Lizenz:</p>
 <ul>
   <li>Eine <a href="https://github.com/PHPGangsta/GoogleAuthenticator">Bibliothek zur Abwicklung der Google-Authenticator-Anmeldung</a> wurde von <a href="https://www.phpgangsta.de/2-faktor-authentifizierung-mit-dem-google-authenticator">Michael Kliewe (PHPGangsta)</a> übernommen und ist unter der BSD-Lizenz freigegeben</li>
