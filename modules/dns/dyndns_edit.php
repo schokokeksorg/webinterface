5fdbeb8eae2c1fad8488f89c5f41b268681c20d1
Hanno Böck <990588+hannob@users.noreply.github.com>
2024-01-06 10:52:53 +0100

    Rechtschreibfehler, Schreibweise HTTPs -> HTTPS

--- modules/dns/dyndns_edit.php
+++ modules/dns/dyndns_edit.php
@@ -36,7 +36,7 @@ if (isset($dyndns['handle'])) {
 
 $http_update_info = '';
 if (isset($dyndns['password'])) {
-    $http_update_info = ' Lassen Sie das Passworteingabefeld unberührt um das bestehende Passwort zu erhalten. Entfernen Sie das bestehende Passwort um das HTTP-Update zu deaktivieren.';
+    $http_update_info = ' Lassen Sie das Passworteingabefeld unberührt, um das bestehende Passwort zu erhalten. Entfernen Sie das bestehende Passwort, um das HTTP-Update zu deaktivieren.';
 }
 
 
@@ -70,13 +70,13 @@ if (!$new) {
 
 $form = '<p><label for="handle">Bezeichnung:</label>&#160;<input type="text" name="handle" id="handle" value="' . ($dyndns['handle'] ?? '') . '" onkeyup="updateUsernameHTTP()" /></p>
 
-<h4>Update per HTTPs</h4>
-<p style="margin-left: 2em;">Geben Sie hier ein Passwort ein um das Update per HTTPs zu aktivieren.' . $http_update_info . '</p>
+<h4>Update per HTTPS</h4>
+<p style="margin-left: 2em;">Geben Sie hier ein Passwort ein, um das Update per HTTPS zu aktivieren.' . $http_update_info . '</p>
 <p style="margin-left: 2em;">Benutzername:&#160;<strong><span id="username_http">' . $username_http . '</span></strong></p>
 <p style="margin-left: 2em;"><label for="password_http">Passwort:</label>&#160;<input type="password" id="password_http" name="password_http" value="' . (isset($dyndns['password']) ? '************' : '') . '" autocomplete="new-password"></p>
 
 <h4>Update per SSH</h4>
-<p style="margin-left: 2em;">Kopieren Sie Ihren SSH-public-Key im OpenSSH-Format in dieses Eingabefeld um das Update per SSH zu aktivieren.</p>
+<p style="margin-left: 2em;">Kopieren Sie Ihren SSH-Public-Key im OpenSSH-Format in dieses Eingabefeld, um das Update per SSH zu aktivieren.</p>
 <p style="margin-left: 2em; vertical-align: middle;"><label for="sshkey">SSH Public-Key:</label><br /><textarea style="height: 10em; width: 80%;" id="sshkey" name="sshkey">' . ($dyndns['sshkey'] ?? '') . '</textarea></p>
 
 <p style="margin-left: 2em;"><input type="submit" value="Speichern" /></p>
