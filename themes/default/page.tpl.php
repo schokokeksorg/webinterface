c1469390fdb9b4d0113dc815ab0a69d43de1bd9a
Hanno Böck <990588+hannob@users.noreply.github.com>
2024-01-27 13:23:26 +0100

    add charset to html so we can validate html without http headers

--- themes/default/page.tpl.php
+++ themes/default/page.tpl.php
@@ -12,7 +12,7 @@ Nevertheless, in case you use a significant part of this code, we ask (but not r
 */
 ?><!DOCTYPE html>
 <html lang="de">
-<head>
+<head><meta charset="UTF-8">
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
