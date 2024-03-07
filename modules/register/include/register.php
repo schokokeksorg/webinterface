747e001e4e56da6cde7784dc5c982ec1fdd331a4
Hanno Böck <990588+hannob@users.noreply.github.com>
2024-03-07 11:14:09 +0100

    Remove unused/duplicate send_mail/send_customer_token functions

--- modules/register/include/register.php
+++ modules/register/include/register.php
@@ -11,8 +11,6 @@ This code is published under a 0BSD license.
 Nevertheless, in case you use a significant part of this code, we ask (but not require, see the license) that you keep the authors' names in place and return your changes to the public. We would be especially happy if you tell us what you're going to do with this code.
 */
 
-require_once('mail.php');
-
 function customer_with_email($email)
 {
     $email = db_escape_string($email);
